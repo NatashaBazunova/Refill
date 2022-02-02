ffd745920d53d81cc6d34007f7754148cd2761a8
NatashaBazunova <natashaiosifova@mail.ru>
2022-02-02 17:22:21 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //данные
+        int amount = 43;
+        int replenishmentAmount = 1550;
+        //процесс
+        int total = amount + replenishmentAmount;
+        int bonus;
+        if (replenishmentAmount > 1000) {
+            bonus = 1;
+            int totalWithBonus = replenishmentAmount / 100 + amount + replenishmentAmount;
+            System.out.println("На Вашем счете:" + totalWithBonus);
+        } else {
+            System.out.println("На Вашем счете:" + total);
+        }
+    }
+}
